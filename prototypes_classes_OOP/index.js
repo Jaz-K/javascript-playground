dfd381e7b6a27e833aac2d4ee2b8be5e7392d793
Jasmin Köhler <jasminangelique@gmail.com>
2023-01-19 11:25:59 +0100

    factory color function

---
+++ prototypes_classes_OOP/index.js
@@ -0,0 +1,37 @@
+// alert("connected");
+/* COLOR TYPE CHANGER  RGB to HEX*/
+
+function hex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function rgb(r, g, b) {
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+hex(255, 23, 15);
+
+/*  FACTORY Function */
+
+function makeColor(r, g, b) {
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+
+    color.rgb = function () {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+
+    color.hex = function () {
+        const { r, g, b } = this;
+        return (
+            "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+        );
+    };
+
+    return color;
+}
+
+const firstColor = makeColor(255, 50, 10);
