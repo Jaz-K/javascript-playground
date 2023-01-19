7629962c659fd12c44d7711481d463270c4c299e
Jasmin Köhler <jasminangelique@gmail.com>
2023-01-19 12:52:14 +0100

    constructor color function

--- prototypes_classes_OOP/index.js
+++ prototypes_classes_OOP/index.js
@@ -35,3 +35,30 @@ function makeColor(r, g, b) {
 }
 
 const firstColor = makeColor(255, 50, 10);
+firstColor.hex(); // '#ff320a'
+firstColor.rgb(); // 'rgb(255, 50, 10)'
+
+/* CONSTRUCTOR */
+
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+
+Color.prototype.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+Color.prototype.hex = function () {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+Color.prototype.rgba = function (a = 1.0) {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b}, ${a})`;
+};
+
+const color1 = new Color(20, 30, 40);
